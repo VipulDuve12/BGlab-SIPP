493993b8f5e259cf6ae02b22d7992d33f7a1cb88
Tejprakashsing <tejgps31@gmail.com>
2025-06-12 09:56:51 +0530

    new files added

--- coreJava_practice_Problems-Sheet-1/AveragePCM.java
+++
@@ -1,9 +0,0 @@
-public class AveragePCM {
-    public static void main(String[] args) {
-        int maths = 94;
-        int phy = 95;
-        int chem = 96;
-        double average = (maths + phy + chem) / 3;
-        System.out.println("Sam’s average mark in PCM is " + average);
-    }
-}

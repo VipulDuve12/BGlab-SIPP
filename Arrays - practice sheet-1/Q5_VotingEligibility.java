6b73a57c4c552bd723b7fde0641b1b1d3982c6f7
Vipul Duve <vduve123@gmail.com>
2025-06-16 10:05:08 +0530

    {VipulDuve12}Add Q5_VotingEligibility.java

--- Arrays - practice sheet-1/Q5_VotingEligibility.java
+++ Arrays - practice sheet-1/Q5_VotingEligibility.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-
-public class Q5_VotingEligibility {
+class Q5_VotingEligibility {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] ages = new int[10];

eab58450e4e00dd0a40349f294f92f7b4226d871
Vipul Duve <vduve123@gmail.com>
2025-06-16 10:04:53 +0530

    {VipulDuve12}Add Q4_OddEvenArrays.java

--- Arrays - practice sheet-1/Q4_OddEvenArrays.java
+++ Arrays - practice sheet-1/Q4_OddEvenArrays.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-
-public class Q4_OddEvenArrays {
+class Q4_OddEvenArrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a natural number: ");

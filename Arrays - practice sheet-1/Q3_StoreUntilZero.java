7864ebd53fc7f5af95cf692dd48a3b5f3f90bc0e
Vipul Duve <vduve123@gmail.com>
2025-06-16 10:04:35 +0530

    {VipulDuve12}AddQ3_StoreUntilZero.java

--- Arrays - practice sheet-1/Q3_StoreUntilZero.java
+++ Arrays - practice sheet-1/Q3_StoreUntilZero.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-
-public class Q3_StoreUntilZero {
+class Q3_StoreUntilZero {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double[] values = new double[10];

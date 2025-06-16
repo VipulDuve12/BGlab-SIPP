3954bf195ec37625f9b025a897bc7cc565350e47
Vipul Duve <vduve123@gmail.com>
2025-06-16 10:16:51 +0530

    {VipulDuve12}Add Q6_MultiplicationTable.java

--- Arrays - practice sheet-1/Q6_MultiplicationTable.java
+++ Arrays - practice sheet-1/Q6_MultiplicationTable.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-
-public class Q6_MultiplicationTable {
+class Q6_MultiplicationTable {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");

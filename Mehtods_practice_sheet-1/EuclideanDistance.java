6d0c188da5a26aa252738d4a1e65fc61e79fcf3c
Vipul Duve <vduve123@gmail.com>
2025-06-17 10:19:49 +0530

    {VipulDuve12} Add

--- Mehtods_practice_sheet-1/EuclideanDistance.java
+++ Mehtods_practice_sheet-1/EuclideanDistance.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-
-public class EuclideanDistance {
+ class EuclideanDistance {
     public static double calculateDistance(double x1, double y1, double x2, double y2) {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
